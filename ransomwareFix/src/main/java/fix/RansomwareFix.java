6cc6614ea56dc1fcd6bf4faeb2a969766986b090
jkoogi <jkoogi@naver.com>
2021-10-10 23:56:11 +0900

    복구파일 이동 후 빈 복구디렉토리 삭제 복구파일 이동 방식 변경(File.renameTo > Files.move) 숨김폴더 탐색 제외

--- ransomwareFix/src/main/java/fix/RansomwareFix.java
+++ ransomwareFix/src/main/java/fix/RansomwareFix.java
@@ -2,27 +2,34 @@ package main.java.fix;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class RansomwareFix {
 
-	private static String DIR_PATH_BASE = "/share/CACHEDEV1_DATA";
-	private static String DIR_PATH_RESCUE = "/share/rescue/restore1/share/CACHEDEV1_DATA";
+	private static String DIR_PATH_BASE = "/share/CACHEDEV1_DATA/";
+	private static String DIR_PATH_RESCUE = "/share/external/DEV3301_1/restore1/share/CACHEDEV1_DATA/";
 	private static String EXTENTION = ".7z";
 	private static String README_FILE = "!!!READ_ME.txt";
 	
 	public static void main(String[] args) {
 		
-		System.out.println("### RansomwareFix START ###");
+		System.out.println("### RansomwareFix START ### ");
 		
 		if(args.length > 2 ) {
 			
-			System.out.println("param set...");
+			System.out.println("param set : "+args.length);
 			
 			DIR_PATH_BASE = args[0];
 			DIR_PATH_RESCUE = args[1];
 			if(args.length > 3) {
 				EXTENTION = args[2];
 			}
+		}else {
+			System.out.println("param is default.");
 		}
 		
 		System.out.println(" - DIR_PATH_BASE : "+DIR_PATH_BASE);
@@ -44,27 +51,70 @@ public class RansomwareFix {
 		if(filse == null) return;
 		for (File file : filse) {
 			if(file.isDirectory()) {
-				showFilesInDirRecursive(file.getPath());
-				isReadmeDelete(file.getPath());
+				
+				// root depth에 [.]로 시작하는 숨김폴더 제외
+				if(file.getPath().startsWith(DIR_PATH_BASE+".")) {
+					System.out.println(" - [~] hidden dir : "+file);
+					continue;
+				}else {
+					
+					showFilesInDirRecursive(file.getPath());
+					isReadmeDelete(file.getPath());
+					emptyRescueDirRemove(file.getPath());
+				}
+				
 			}else if (file.getName().endsWith(".7z")) {
+				
 				System.out.println("file : " + file);
 				File rescueFile = getRescueFile(file); 
+				
 				if(rescueFile != null && rescueFile.exists()) {
+					System.out.println(" - rescueFile exists : "+rescueFile);
 					moveRescuFile(file, rescueFile);
+				}else {
+					System.out.println(" - rescueFile exists : NO");
 				}
 			}
 		}
 	}
 
+	// 복구파일 이동 후 빈 복구디렉토리 삭제
+	private static void emptyRescueDirRemove(String path) {
+		
+		File rescueDir = new File(path.replace(DIR_PATH_BASE, DIR_PATH_RESCUE));
+		if(rescueDir.exists()) {
+			
+			File files[] = rescueDir.listFiles();
+			
+			if(files.length==0) {
+				if(rescueDir.delete()) {
+					System.out.println(" - remove rescue dir : "+rescueDir);
+				}else {
+					System.out.println(" - fail remove rescue dir : "+rescueDir);
+				}
+			}
+		}
+	}
 
 	// 복구파일 이동
 	private static void moveRescuFile(File file, File rescueFile) {
 		
-		File newFile = new File(file.getParent()+"/"+rescueFile.getName());
-		boolean success = rescueFile.renameTo(newFile);
-		if (success) {
-		    System.out.println("move newFile : "+newFile);
-		    file.delete();
+		Path rescuePath = Paths.get(rescueFile.getPath());
+		Path newPath = Paths.get(file.getParent()+"/"+rescueFile.getName());
+		
+		try {
+			Path path = Files.move(rescuePath, newPath, StandardCopyOption.REPLACE_EXISTING);
+			System.out.println("path :" + path);
+			
+			if (path.toFile().exists()) {
+			    System.out.println(" -> [*] move newFile : "+path.toFile());
+			    file.delete();
+			} else {
+				System.out.println(" -> [!] move fail : "+file.getParent()+"/"+rescueFile.getName()+" | file.getParent() : "+file.getParent());
+			}
+		} catch (IOException e) {
+			System.out.println(" -> [!] move fail : "+file.getParent()+"/"+rescueFile.getName()+" | file.getParent() : "+file.getParent());
+			e.printStackTrace();
 		}
 	}
 
@@ -76,6 +126,9 @@ public class RansomwareFix {
 		String rescuePath = parantPath.replace(DIR_PATH_BASE, DIR_PATH_RESCUE);
 		
 		File rescueFile = new File(rescuePath+"/"+fileName);
+		System.out.println(" - getRescueFile.file.org : "+file);
+		System.out.println(" - getRescueFile.file.new : "+rescueFile);
+		
 		return rescueFile;
 	}
 
