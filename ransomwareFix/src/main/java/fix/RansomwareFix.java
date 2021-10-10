06320b07f7c63511bd273b917f0902aa64914825
jkoogi <jkoogi@naver.com>
2021-10-10 15:00:23 +0900

    복구파일 조회

--- ransomwareFix/src/main/java/fix/RansomwareFix.java
+++ ransomwareFix/src/main/java/fix/RansomwareFix.java
@@ -6,8 +6,10 @@ import java.io.FilenameFilter;
 
 public class RansomwareFix {
 
-	private static final String baseDirPath = "D:\\dev\\_log\\_ransomewareTest";
+	private static final String baseDirPath = "D:\\dev\\_log\\_ransomewareTest\\WD";
+	private static final String rescueDirPath = "D:\\dev\\_log\\_ransomewareTest\\rescue\\restore1\\share\\CACHEDEV1_DATA";
 	private static final String README_FILE = "!!!READ_ME.txt";
+	private static final String EXTENTION = ".7z";
 	
 	public static void main(String[] args) {
 		
@@ -31,8 +33,27 @@ public class RansomwareFix {
 				showFilesInDirRecursive(file.getPath());
 			}else if (file.getName().endsWith(".7z")) {
 				System.out.println("file : " + file);
+				File rescueFile = getRescueFile(file); 
+				if(rescueFile != null && rescueFile.exists()) {
+					System.out.println("move file : "+file);
+//					moveRescuFile(file, rescueFile);
+//					deleteRansomwareFile(file);
+				}
 			}
 		}
+	}
+
+
+	// 복구파일 조회
+	private static File getRescueFile(File file) {
+		System.out.println(" > file : " + file);
+		
+		String fileName = file.getName().substring(0,file.getName().length()-EXTENTION.length());
+		String parantPath = file.getParentFile().toString();
+		String rescuePath = parantPath.replace(baseDirPath, rescueDirPath);
+		
+		File rescueFile = new File(rescuePath+"/"+fileName);
+		return rescueFile;
 	}
 
 	// 디렉토리에 7z파일이 없으면 ReadMe 파일 삭제 
@@ -42,7 +63,7 @@ public class RansomwareFix {
 		FilenameFilter filter = new FilenameFilter() {
 			@Override
 			public boolean accept(File dir, String name) {
-				return name.endsWith("7z");
+				return name.endsWith(".7z");
 			}
 		};
 		
@@ -50,9 +71,7 @@ public class RansomwareFix {
 		File readmeFile = new File(path+"/"+README_FILE);
 		if(files.length==0 && readmeFile.isFile()) {
 			System.out.println(">> delete ReadMe : "+readmeFile);
-			// TODO 파일삭제
-//			readmeFile.delete();
+			readmeFile.delete();
 		}
-		
 	}
 }
