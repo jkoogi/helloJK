ff0764d9f7dee2f9c3c31ca6efa8060e5d558b4f
jkoogi <jkoogi@naver.com>
2022-01-26 21:09:56 +0900

    1.1 프로젝트 생성 실습코드

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,7 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+		
+		/* lombok 테스트 
+		Hello hello = new Hello();
+		hello.setName("hello");
+		String name = hello.getName();
+		
+		System.out.println("name : " + name);
+		 * */
+		
 		SpringApplication.run(JpashopApplication.class, args);
+		
 	}
 
 }
