a386dbe6cf1aa4fd04729f079cbd4a5a53520db8
jkoogi <jkoogi@naver.com>
2022-03-16 00:01:08 +0900

    * 엔티티 클래스 개발2 실습 - 값타입 선언시 변경 불능으로 설계

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,10 +6,19 @@ import lombok.Getter;
 
 @Embeddable
 @Getter
-public class Address {
+public class Address {// Class 'Address' should have public no-arg constructor
 
 	private String city;
 	private String street;
 	private String zipcode;
 
+	/* jpa spec에서 @Entity, @Embeddable 사용시 테이블 매핑클래스를 명시함 - 기본생성자 필수 
+	 * - 값타입 선언시 변경 불능으로 설계 - 생성자로만 값 설정시 오류? 가 왜 안나지?
+	 * > jpa에서 reflaction, proxy 적용을 위한 생성자 정의 : protected 생성자 허용
+	 * */
+	public Address(String city, String street, String zipcode) {
+		this.city = city;
+		this.street = street;
+		this.zipcode = zipcode;
+	}
 }
