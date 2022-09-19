a02e3d998c02bf3dd438742a237b3ed3c93900af
jkoogi <jkoogi@naver.com>
2022-09-19 21:47:25 +0900

    Update ScoreCollectionTest.java
    오타수정

--- JavaJUnitTest/test/iloveyouboss/ex130/ScoreCollectionTest.java
+++ JavaJUnitTest/test/iloveyouboss/ex130/ScoreCollectionTest.java
@@ -12,7 +12,7 @@ public class ScoreCollectionTest {
 	public void answersArithmeticMeanOfTwoNumbers() {
 		//준비(arrange) : 인스턴스 생성 후 구현객체 설정
 		ScoreCollection collection = new ScoreCollection();
-		collection.add(()->5); // getScore 메서드가 반환할 겂을 설정할수 있게 하는 구현을 적용할 수 있지만, 별도의 불필요한 코드가 필요하여 
+		collection.add(()->5); // getScore 메서드가 반환할 값을 설정할수 있게 하는 구현을 적용할 수 있지만, 별도의 불필요한 코드가 필요하여 
 		collection.add(()->7); // 단순한 람다표현식으로 Scoreable 인스턴스에서 값을 반환하도록 지정 [()->7]
 		
 		//실행(act)
